c5771c379c5063dd207e788d9d1b9ba956c6b70a
henrykash <henrykariuki003@gmail.com>
2023-07-07 17:41:54 +0300

    building a web server: hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::routing::get;
+use axum::{Router, response::Html};
+
+#[tokio:: main]
+
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async {Html("Hello, <strong> world!! </strong>" )}),
+    );
+
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on  {addr}\n");
+
+    // Run server
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+    
 }
