f2917f08ef28b435106348c19826c00df8010c3f
henrykash <henrykariuki003@gmail.com>
2023-07-07 17:46:58 +0300

    building a web server: hello world

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use axum::{Router, response::Html};
 #[tokio:: main]
 
 async fn main() {
+
+     //create a route for /hello and return a string "Hello, world!"
     let routes_hello = Router::new().route(
         "/hello",
         get(|| async {Html("Hello, <strong> world!! </strong>" )}),
     );
 
-
+    // Create a socket address to listen on 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on  {addr}\n");
 
