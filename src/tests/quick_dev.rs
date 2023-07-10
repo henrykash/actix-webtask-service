52726377e0f7b09a1ab54f60a3093d0dbc4d9199
henrykash <henrykariuki003@gmail.com>
2023-07-10 12:02:10 +0300

    I have added a tests file for tests

--- src/tests/quick_dev.rs
+++ src/tests/quick_dev.rs
@@ -3,7 +3,6 @@ use anyhow::Result;
 #[toko::test] 
 async fn quick_dev()-> Result<()> {
     let hc = httpc::new_client("http://localhost:8080")?;
-
     hc.do_get("/hello").await?.print().await?; // returns a response
 
     Ok(())
